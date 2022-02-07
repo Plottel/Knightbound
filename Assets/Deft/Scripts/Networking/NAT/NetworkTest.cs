30d1b06b54688f5ba78b281fb7679c1fee9ce2d8
Matt <matt.noone95@gmail.com>
2022-02-08 10:07:27 +1100

    Add Native Web Socket Plugin for communicating with AWS

--- Assets/Deft/Scripts/Networking/NAT/NetworkTest.cs
+++ Assets/Deft/Scripts/Networking/NAT/NetworkTest.cs
@@ -1,15 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using NativeWebSocket;
 
 public class NetworkTest : MonoBehaviour
 {
-    private const string wwwURI = "https://izo74v7osh.execute-api.us-east-1.amazonaws.com/default/HelloWorld";
+    const string wwwURI = "https://izo74v7osh.execute-api.us-east-1.amazonaws.com/default/HelloWorld";
+    const string webSocketURI = "wss://b1y9fhlqtj.execute-api.us-east-1.amazonaws.com/production";
+
+    private WebSocket natSocket;
 
     private void Start()
     {
-        StartCoroutine(TickWebRequest());
+        natSocket = new WebSocket(webSocketURI);
+        natSocket.OnOpen += OnNATSocketOpen;
+        natSocket.OnMessage += OnNATSocketMessage;
+
+        ConnectSocket();
+
+        //StartCoroutine(TickWebRequest());
+    }
+
+    async void ConnectSocket()
+    {
+        await natSocket.Connect();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SendSocketMessage("Ping");
+
+        Debug.Log("Socket State: " + natSocket.State.ToString());
+    }
+
+    async void SendSocketMessage(string message)
+    {
+        await natSocket.SendText(message);
+    }
+
+    private void OnNATSocketMessage(byte[] data)
+    {
+        string message = Encoding.UTF8.GetString(data);
+        Debug.Log("Socket Msg: " + message);
+    }
+
+    private void OnNATSocketOpen()
+    {
+        Debug.Log("Successfully opened NAT Socket!");
     }
 
     IEnumerator TickWebRequest()
